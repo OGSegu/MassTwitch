30166ebc1f86581d5b52960d2cb38306491a69e7
OGSegu <seguisreal@gmail.com>
2020-08-29 22:22:33 +0300

    Amount of threads fixed

--- src/main/java/twitch/Checkable.java
+++ src/main/java/twitch/Checkable.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 public abstract class Checkable {
 
     final ThreadPoolExecutor executor = new ThreadPoolExecutor(0,
-            100,
+            Runtime.getRuntime().availableProcessors(),
             4L,
             TimeUnit.SECONDS,
             new SynchronousQueue<>(),
